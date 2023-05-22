57617f8e95cb6379183b244ed6d5caccc0127d3c
ZidanYohanza <Mohammad21005@mail.unpad.ac.id>
2023-05-22 23:49:00 +0700

    Code Algorithm Prims

--- algorithmPrims.cpp
+++ algorithmPrims.cpp
@@ -1,106 +1,80 @@
-// A C++ program for Prim's Minimum
-// Spanning Tree (MST) algorithm. The program is
-// for adjacency matrix representation of the graph
-
-#include <bits/stdc++.h>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
-// Number of vertices in the graph
-#define V 5
+#define INF 9999999
 
-// A utility function to find the vertex with
-// minimum key value, from the set of vertices
-// not yet included in MST
-int minKey(int key[], bool mstSet[])
-{
-	// Initialize min value
-	int min = INT_MAX, min_index;
+// jumlah simpul dalam grafik
+#define V 9
 
-	for (int v = 0; v < V; v++)
-		if (mstSet[v] == false && key[v] < min)
-			min = key[v], min_index = v;
+// membuat array 2d ukuran 9x9
+// untuk matriks adjacenckolom untuk mewakili grafik
 
-	return min_index;
+int graph[V][V] = { { 0, 4, 9, 0, 0, 0, 0, 0, 0  },
+				    { 4, 0, 11, 0, 8, 0, 0, 0, 0 },
+                    { 9, 11, 0, 7, 0, 1, 0, 0, 0 },
+                    { 0, 0, 7, 0, 2, 6, 0, 0, 0  },
+                    { 0, 8, 0, 2, 0, 0, 7, 4, 0  },
+                    { 0, 0, 1, 6, 0, 0, 0, 2, 0  },
+                    { 0, 0, 0, 0, 7, 0, 0, 14, 9 },
+                    { 0, 0, 0, 0, 4, 2, 14, 0, 10},
+                    { 0, 0, 0, 0, 0, 0, 9, 10, 0 } 
+                };
+
+int main() {
+    int no_edge;  // jumlah sisi
+
+    // buat array untuk melacak simpul yang dipilih
+    // pemilihan akan menjadi benar jika tidak salah
+    int key[V];
+
+    // inisiasi false yang dipilih pada awalnya
+    memset(key, false, sizeof(key));
+
+    // inisiasi jumlah tepi ke 0
+    no_edge = 0;
+
+    // jumlah sisi dalam MST akan 
+    // selalu lebih kecil dari (V -1), dimana V adalah jumlah simpul dalam 
+    //grafik
+
+    // pilih simpul ke-0 dan jadikan itu benar
+    key[0] = true;
+
+    int baris;  //  Baris
+    int kolom;  //  Kolom
+
+    // print for edge and weight
+    cout << "Sisi" << " : " << "Bobot" << endl;
+
+    while (no_edge < V - 1) {
+        //Untuk setiap simpul di himpunan S, temukan semua simpul yang bertetangga
+        // , hitung jarak dari simpul yang dipilih pada langkah 1.
+        // jika vertex sudah ada di set S, buang jika tidak
+        //pilih simpul lain yang terdekat dengan simpul terpilih pada langkah 1.
+
+        int min = INF;
+        baris = 0;
+        kolom = 0;
+
+        for (int i = 0; i < V; i++) {
+            if (key[i]) {
+                for (int j = 0; j < V; j++) {
+                    if (!key[j] && graph[i][j]) {  // tidak dipilih dan ada sisi
+                        if (min > graph[i][j]) {
+                            min = graph[i][j];
+                            baris = i;
+                            kolom = j;
+                        }
+                    }
+                }
+            }
+        }
+        cout << baris << " - " << kolom << " :  " << graph[baris][kolom];
+        cout << endl;
+        key[kolom] = true;
+        no_edge++;
+    }
+
+    return 0;
 }
-
-// A utility function to print the
-// constructed MST stored in parent[]
-void printMST(int parent[], int graph[V][V])
-{
-	cout << "Edge \tWeight\n";
-	for (int i = 1; i < V; i++)
-		cout << parent[i] << " - " << i << " \t"
-			<< graph[i][parent[i]] << " \n";
-}
-
-// Function to construct and print MST for
-// a graph represented using adjacency
-// matrix representation
-void primMST(int graph[V][V])
-{
-	// Array to store constructed MST
-	int parent[V];
-
-	// Key values used to pick minimum weight edge in cut
-	int key[V];
-
-	// To represent set of vertices included in MST
-	bool mstSet[V];
-
-	// Initialize all keys as INFINITE
-	for (int i = 0; i < V; i++)
-		key[i] = INT_MAX, mstSet[i] = false;
-
-	// Always include first 1st vertex in MST.
-	// Make key 0 so that this vertex is picked as first
-	// vertex.
-	key[0] = 0;
-
-	// First node is always root of MST
-	parent[0] = -1;
-
-	// The MST will have V vertices
-	for (int count = 0; count < V - 1; count++) {
-		
-		// Pick the minimum key vertex from the
-		// set of vertices not yet included in MST
-		int u = minKey(key, mstSet);
-
-		// Add the picked vertex to the MST Set
-		mstSet[u] = true;
-
-		// Update key value and parent index of
-		// the adjacent vertices of the picked vertex.
-		// Consider only those vertices which are not
-		// yet included in MST
-		for (int v = 0; v < V; v++)
-
-			// graph[u][v] is non zero only for adjacent
-			// vertices of m mstSet[v] is false for vertices
-			// not yet included in MST Update the key only
-			// if graph[u][v] is smaller than key[v]
-			if (graph[u][v] && mstSet[v] == false
-				&& graph[u][v] < key[v])
-				parent[v] = u, key[v] = graph[u][v];
-	}
-
-	// Print the constructed MST
-	printMST(parent, graph);
-}
-
-// Driver's code
-int main()
-{
-	int graph[V][V] = { { 0, 2, 0, 6, 0 },
-						{ 2, 0, 3, 8, 5 },
-						{ 0, 3, 0, 0, 7 },
-						{ 6, 8, 0, 0, 9 },
-						{ 0, 5, 7, 9, 0 } };
-
-	// Print the solution
-	primMST(graph);
-
-	return 0;
-}
-
-// This code is contributed by rathbhupendra
